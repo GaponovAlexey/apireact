4b91855fd42430ecb74a966d1e817ef4718a84d6
Kekelberi <alexei.gaponov@gmail.com>
2021-08-18 16:17:29 +0300

    save

--- src/pages/TwoConteiner.jsx
+++ src/pages/TwoConteiner.jsx
@@ -1,39 +1,41 @@
 import React, { useEffect, useReducer, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import PageTwo from './PageTwo';
 import getRepos from './../api/repos';
 import { Context } from './context';
 import reducer from './reducer';
+import PageTwo from './pagetwo';
 
 
 const TwoConteiner = () => {
+
 	const [todo, setTodo] = useState([
-		{ id: 1, title: 'one', completed: false },
-		{ id: 2, title: 'two', completed: false }
+		{id: 1, title: 'hi povtorjalka', completed: false},
+		{id: 2, title: 'hi js', completed: false},
+		{id: 3, title: 'hi react', completed: false}
 	])
-	const [useTodo, setUseTodo] = useState()
+	const [todouse, setTodoUse] = useState()
+
 	const addTodo = e => {
 		if (e.key === 'Enter') {
 			setTodo([
 				...todo, {
 					id: Date.now(),
-					title: useTodo,
-					completed: false
+					title: todouse,
+					completed: false,
 				}
 			])
-			setUseTodo('')
 		}
 	}
-
 	return (
 		<div>
 			<PageTwo todos={ todo } />
 			<div>
 				<input
 					type='text'
-					value={ useTodo }
-					onChange={ (e) => setUseTodo(e.target.value) }
+					value={todouse}
+					onChange={(e) => setTodoUse(e.target.value)}
 					onKeyPress={addTodo}
+					
 				/>
 			</div>
 		</div>
